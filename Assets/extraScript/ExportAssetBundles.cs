3fa7bd77c3f09983313632f2471beae9f2015e6d
Patrick KWEE Chun Yoong <cy.patrickwee@gmail.com>
2017-02-24 15:31:09 +0800

    adding the entire source

---
+++ Assets/extraScript/ExportAssetBundles.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+/*public class ExportAssetBundles : Editor {
+    [MenuItem("Assets/Build AssetBundle")]
+    static void ExportResource()
+    {
+        string path = "Assets/AssetBundle/myAssetBundle.unity3d";
+        Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+        BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets,BuildTarget.Android);
+    }
+}*/
+
+// C# Example
+// Builds an asset bundle from the selected objects in the project view.
+// Once compiled go to "Menu" -> "Assets" and select one of the choices
+// to build the Asset Bundle
+public class BuildAssetBundlesExample : MonoBehaviour
+{
+    [MenuItem("Example/Build Asset Bundles")]
+    static void BuildABs()
+    {
+        string path = "Assets/AssetBundle/";
+
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        // Put the bundles in a folder called "ABs" within the Assets folder.
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/extraScript/scriptAsset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+class scriptAsset : MonoBehaviour
+{
+    public static readonly string BundleURL = "https://dl.dropboxusercontent.com/s/z7atujcvd83tizx/backdrop.1?dl=0";
+
+    private string AssetName = "backdrop";
+
+    IEnumerator Start()
+    {
+        using (WWW www = new WWW(BundleURL))
+        {
+            yield return www;
+            if (www.error != null)
+                throw new Exception("WWW download had an error:" + www.error);
+            AssetBundle bundle = www.assetBundle;
+            GameObject cube = Instantiate(bundle.LoadAsset(AssetName)) as GameObject;
+            cube.transform.position = new Vector3(0f, 0f, 0f);
+            bundle.Unload(false);
+
+        }
+    }
+}
